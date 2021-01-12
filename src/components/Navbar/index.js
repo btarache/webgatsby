a603f2dd48a50794e715dc5e1f8745397c217e00
btarache <benjamintarache@gmail.com>
2021-01-12 13:39:44 -0600

    Navbar, estilos, agregados

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,7 +6,11 @@ import {
     Nav,
     NavbarContainer,
     NavIcon,
-    NavLogo
+    NavItem,
+    NavLinks,
+    NavLogo,
+    NavMenu,
+    
 } from './NavbarElements';
 
 const Navbar = () => {
@@ -42,6 +46,17 @@ const Navbar = () => {
                         <MobileIcon onClick={handleClick}>
                             {click ? <FaTimes /> : <FaBars />}
                         </MobileIcon>
+                        <NavMenu onClick={handleClick} click={click}>
+                            <NavItem>
+                                <NavLinks to='/'>Home</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to='/images'>Images</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to='/destinations'>Destinations</NavLinks>
+                            </NavItem>
+                        </NavMenu>
                     </NavbarContainer>
                 </Nav>
             </IconContext.Provider>
